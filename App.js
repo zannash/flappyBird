00625982416560d6adff8a03d8832dafd784204b
zannash <zanaashehu@gmail.com>
2024-04-17 15:21:37 +0200

    Module 25

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import Bird from './src/components/Bird';
 
 export default function App() {
+  const screenWidth = Dimensions.get("screen").width;
+  const screenHeight = Dimensions.get("screen").height;
+  const birdLeft = screenWidth/2;
+  const [birdBottom,setBirdBottom] = useState(screenHeight/2);
+  const gravity = 3;
+  let gameTimerId;
+
+  useEffect(()=> {
+
+        if(birdBottom > 0){
+      gameTimerId = setInterval(()=>{
+        setBirdBottom(birdBottom => birdBottom - gravity),30
+      })
+      return() => {
+        clearInterval(gameTimerId);
+      }
+        }
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View style={styles.container}
+   >
+    <Bird/>
+   </View>
   );
 }
 
